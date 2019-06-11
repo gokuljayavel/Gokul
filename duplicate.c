33739355bda22765a234c2927f7c782e3379ca24
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2019-06-11 12:23:32 +0530

    Update duplicate.c

--- duplicate.c
+++ duplicate.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-	long long int num,i,l=0;
+	long long int num,i,l=0,f=0;
 	scanf("%lld",&num);
 	long long int arr[num],c[100]={0};
 	for(i=0;i<num;i++){
@@ -9,10 +9,13 @@ int main(void) {
 	c[arr[i]]++;}
 	for(i=0;i<100;i++){
 		if(c[i]>1 && l==0){
+			f=1;
 		printf("%lld",i);
 		l=1;}
 		else if(c[i]>1)
 		printf(" %lld",i);
 	}
+	if(f==0)
+	printf("unique");
 	return 0;
 }
