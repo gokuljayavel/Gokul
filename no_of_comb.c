153c45500bd84593a1bf4905667f20f4847a3a52
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2019-06-11 13:23:17 +0530

    Update no_of_comb.c

--- no_of_comb.c
+++ no_of_comb.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-	int n,i=0,j,t1=0,t2=1,t3;
-	char a[1001];
-	scanf("%s%n",a,&n);
-	for(i=n-1;i>0;i--){
-		j=((a[i-1]-48)*10+(a[i]-48));
+	int n,i=0,j,t1=0,t2=1,t3,k=0;
+	int a[1001];
+	scanf("%d",&n);
+	while(n>0){
+		a[k++]=n%10;
+		n=n/10;}
+	for(i=0;i<k-1;i++){
+		j=((a[i+1])*10+(a[i]));
 		if(j>=1 && j<=26){
 			t3=t1+t2;
 			t1=t2;
@@ -16,4 +19,3 @@ int main(void) {
 			t2=t3;}}
 	printf("%d",t3);
 	return 0;
-}
