261fe2f87e2d9a928f53d28cb9fc2649f0d38388
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2019-06-11 13:42:20 +0530

    Create kthlargest.c

---
+++ kthlargest.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,k,i,j,t;
+	scanf("%d %d",&n,&k);
+	int a[n];
+	for(i=0;i<n;i++)
+	scanf("%d" ,&a[i]);
+	for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			if(a[i]<a[j]){
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;}}}
+	printf("%d",a[k-1]);
+	return 0;
+}
