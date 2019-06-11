b77b9df2091818f0ad6eddcdeda41f9b5c4d203d
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2019-06-11 10:10:56 +0530

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	long long int num;
+	scanf("%lld",&num);
+	if(!isalpha(num))
+	if(num<0)
+		printf("invalid");
+	else{
+		if(num%2==0)
+		printf("Even");
+		else
+		printf("Odd");}
+	return 0;
+}
