23cecc6d752e88b64eaad772a974bc6726360f8a
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2019-07-05 23:57:42 +0530

    Update childrensort.c

--- childrensort.c
+++ childrensort.c
@@ -7,18 +7,18 @@ int main(void) {
 	for(i=0;i<n;i++){
 		scanf("%d ",&a[i]);
 	}
-	for(i=0;i<n;i++){
-		for(j=i+1;j<n;j++){
-			if(a[i]>a[j]){
-				t=a[i];
-				a[i]=a[j];
-				a[j]=t;}}}
-				s[0]=1;
+		if(a[0]>a[1])
+		s[0]=2;
+		else
+		s[0]=1;
+		c=s[0];
 		for(i=1;i<n;i++){
-			if(a[i]==a[i-1])
-			s[i]=s[i-1];
-			else if(a[i]>a[i-1])
+			if(a[i]>a[i-1])
 			s[i]=s[i-1]+1;
+			else if(a[i]==a[i-1])
+			s[i]=s[i-1];
+			else
+			s[i]=1;
 			c+=s[i];
 		}
 		printf("%d",c);
