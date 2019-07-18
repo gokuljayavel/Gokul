72817fa2448898892ec9f9db4c77d4a33c6d9030
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2019-07-18 21:31:38 +0530

    Update arrstar_superstar.c

--- arrstar_superstar.c
+++ arrstar_superstar.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-	int n,i,j,f=0,l=0,k;
+	int n,i,j,f=0,l=0,m=0;
 	scanf("%d",&n);
 	int a[n];
 	for(i=0;i<n;i++)
 	scanf("%d ",&a[i]);
 	for(i=0;i<n-1;i++){
 		f=0;
+		if(a[i]>m)
+		m=a[i];
 		for(j=i+1;j<n;j++){
 			if(a[i]<a[j]){
 			f=1;
@@ -16,19 +18,6 @@ int main(void) {
 		printf("%d ",a[i]);
 	}
 	printf("%d\n",a[n-1]);
-	for(i=0;i<n;i++){
-		f=0;
-		l=0;
-		for(j=0;j<i-1;j++){
-			if(a[i]<a[j]){
-			f=1;
-			break;}}
-		for(k=i+1;k<n;k++){
-			if(a[i]<a[k]){
-				l=1;
-				break;}}
-		if(f==0 && l==0)
-		printf("%d ",a[i]);
-	}
+	printf("%d",m);
 	return 0;
 }
