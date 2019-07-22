b3f0f3f317e082a48a31c69052db1d207402de72
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2019-07-22 14:33:02 +0530

    Update maxarr.py

--- maxarr.py
+++ maxarr.py
@@ -1,12 +1,12 @@
 n = int(input())
-l = list(map(int,input().split()))
-c = []
-for i in range(len(l)):
-	for j in range(i+1,len(l)+1):
-		s = sum(l[i:j])
-		c.append(s)
+arr = list(map(int,input().split()))
+r = []
+for i in range(n):
+	for j in range(i+1,n+1):
+		s = sum(arr[i:j])
+		r.append(s)
 		s = 0
 for i in l:
-	c.append(i)
-c.sort()
-print(c[-1])
+	r.append(i)
+r.sort()
+print(r[-1])
