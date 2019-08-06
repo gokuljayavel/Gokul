ccf2c7047b6bcc3af0b9827f1d8d41f824c60840
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2019-08-06 20:25:08 +0530

    Update arravg.c

--- arravg.c
+++ arravg.c
@@ -3,18 +3,19 @@
 int main(void) {
 	int i,n,j,lsum=0,rsum=0;
 	scanf("%d",&n);
-	int a[n];
+	int a[n],f=0;
 	for(i=0;i<n;i++)
 	scanf("%d ",&a[i]);
-	for(i=0;i<n;i++){
+	for(i=0;i<n-1;i++){
 		lsum+=a[i];
 		rsum=0;
 		for(j=i+1;j<n;j++)
 			rsum+=a[j];
 		if(lsum*(n-i-1)==rsum*(i+1)){
 		printf("yes");
-		return 0;}
+		f=1;}
 	}
+	if(f==0)
 	printf("no");
 	return 0;
 }
