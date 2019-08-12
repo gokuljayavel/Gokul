e0a01b9f10248f2f6414ece8f98ccc7c7dd08a0f
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2019-08-12 22:31:15 +0530

    Update duplstring.c

--- duplstring.c
+++ duplstring.c
@@ -2,13 +2,12 @@
 
 int main(void) {
 	int n,i,j,a[26]={0};
-	char s[100000];
+	char s[100000]; 
 	scanf("%s",s);
-	for(i=0;s[i]!='\0';i++)
-	 a[s[i]-97]++;
-	 for(i=0;i<26;i++){
-	 	if(a[i]>0)
-	 	printf("%c",i+97);
+	for(i=0;s[i]!='\0';i++){
+	if(a[s[i]-97]==0)
+	printf("%c",s[i]);
+	a[s[i]-97]++;
 	 }
 	return 0;
 }
