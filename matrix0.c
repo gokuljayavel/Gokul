5c2c11dbedf231e3f41916f6d15315e14e81caed
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2019-08-12 11:30:11 +0530

    Create matrix0.c

---
+++ matrix0.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i,m,j,l;
+	scanf("%d %d",&m,&n);
+	int a[m][n],b[m][n];
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+		scanf("%d ",&a[i][j]);
+		b[i][j]=a[i][j];}}
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			if(a[i][j]==0){
+				for(l=0;l<m;l++)
+				b[l][j]=0;
+				for(l=0;l<n;l++)
+				b[i][l]=0;}}}
+	for(i=0;i<m;i++,printf("\n"))
+		for(j=0;j<n;j++)
+			printf("%d ",b[i][j]);
+	return 0;
+}
+
